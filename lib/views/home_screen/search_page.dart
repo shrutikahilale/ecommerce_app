39eb1296a1ebb9dc09bf2bb0ac0cc39c9b4c7d99
Shrutika Kailas Hilale <shrutikahilale8@gmail.com>
2024-04-20 17:32:00 +0530

    search_page fixed

--- lib/views/home_screen/search_page.dart
+++ lib/views/home_screen/search_page.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:ecommerce_app/controllers/home_controller.dart';
 import 'package:ecommerce_app/widgets_common/product_card.dart';
 import 'package:ecommerce_app/widgets_common/product_description_page.dart';
+import '../../widgets_common/bg_widget.dart';
 
 class SearchPage extends StatelessWidget {
   final TextEditingController _searchController = TextEditingController();
@@ -12,76 +13,103 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<HomeController>(builder: (ctrl) {
-      List<Product> filteredProducts = ctrl.products.where((product) {
-        String productName = product.name.toString().toLowerCase();
-        List<String> query = _searchController.text.toLowerCase().split(' ');
-        return query.every((term) => productName.contains(term));
-      }).toList();
+    return bgWidget(
+      child: GetBuilder<HomeController>(builder: (ctrl) {
+        List<Product> filteredProducts = ctrl.products.where((product) {
+          if (_searchController.text.trim().isEmpty) return true;
 
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Search'),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () {
-              Get.back();
-            },
+          String productName = product.name.toString().toLowerCase();
+          List<String> tokens = productName.split(' ');
+          List<String> query = _searchController.text.toLowerCase().split(' ');
+
+          // Check if every term in the query is present in the productName
+          for (String term in query) {
+            bool found = false;
+            for (String token in tokens) {
+              if (term == token) {
+                found = true;
+                break;
+              }
+            }
+            if (!found) {
+              // If any term from the query is not found in productName, return false
+              return false;
+            }
+          }
+
+          // If all terms are found, return true
+          return true;
+        }).toList();
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Search'),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Get.back();
+              },
+            ),
           ),
-        ),
-        body: Container(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                height: 60,
-                child: TextFormField(
-                  controller: _searchController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    suffixIcon: Icon(Icons.search),
-                    hintText: 'Search',
+          body: Container(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.center,
+                  height: 60,
+                  child: TextFormField(
+                    controller: _searchController,
+                    decoration: const InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white, width: 1.0),
+                      ),
+                      suffixIcon: Icon(Icons.search),
+                      hintText: 'Search',
+                    ),
+                    onChanged: (value) {
+                      ctrl.update(); // Rebuild UI on search text change
+                    },
                   ),
-                  onChanged: (value) {
-                    ctrl.update(); // Rebuild UI on search text change
-                  },
                 ),
-              ),
-              Expanded(
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  itemCount: filteredProducts.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    mainAxisSpacing: 8,
-                    crossAxisSpacing: 8,
-                    mainAxisExtent: 300,
+                Expanded(
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    itemCount: filteredProducts.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                      mainAxisExtent: 300,
+                    ),
+                    itemBuilder: (context, index) {
+                      return ProductCard(
+                        name: filteredProducts[index].name ?? 'no name',
+                        imageUrl: filteredProducts[index].image ?? 'url',
+                        price: filteredProducts[index].price ?? 0.00,
+                        offerTag: (filteredProducts[index].offer ?? true)
+                            ? filteredProducts[index].offerPercent ?? ' '
+                            : ' ',
+                        review: filteredProducts[index].review ?? 0.0,
+                        numOfferPercent:
+                            filteredProducts[index].numOfferPercent ?? 0.0,
+                        onTap: () {
+                          Get.to(const ProductDescriptionPage(),
+                              arguments: {'data': filteredProducts[index]});
+                        },
+                        hasOffer: filteredProducts[index].offer ?? false,
+                      );
+                    },
                   ),
-                  itemBuilder: (context, index) {
-                    return ProductCard(
-                      name: filteredProducts[index].name ?? 'no name',
-                      imageUrl: filteredProducts[index].image ?? 'url',
-                      price: filteredProducts[index].price ?? 0.00,
-                      offerTag: (filteredProducts[index].offer ?? true)
-                          ? filteredProducts[index].offerPercent ?? ' '
-                          : ' ',
-                      review: filteredProducts[index].review ?? 0.0,
-                      numOfferPercent:
-                          filteredProducts[index].numOfferPercent ?? 0.0,
-                      onTap: () {
-                        Get.to(const ProductDescriptionPage(),
-                            arguments: {'data': filteredProducts[index]});
-                      },
-                      hasOffer: filteredProducts[index].offer ?? false,
-                    );
-                  },
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      );
-    });
+        );
+      }),
+    );
   }
 }
